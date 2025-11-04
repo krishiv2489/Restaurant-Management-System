fd907ad5f979a595a1ad61f282c0ada4bba73355
krishiv2489 <krishiv248@gmail.com>
2025-11-04 09:36:59 +0530

    Updated project

--- CP_Project.c
+++ CP_Project.c
@@ -6,7 +6,25 @@ int chef();
 int table_manager();
 int login();
 int main() {
-    login();
+    int who = login();
+
+    switch (who) {
+    case 1:
+        owner();
+        break;
+    case 2:
+        chef();
+        break;
+    case 3:
+        waiter();
+        break;
+    case 4:
+        table_manager();
+        break;
+    default:
+        printf("Invalid choice message\n");
+}
+
     return 0;
 }
 int login(){
@@ -22,11 +40,17 @@ int login(){
         "|  2. Login as CHEF           |\n"
         "|  3. Login as WAITER         |\n"
         "|  4. Login as TABLE MANAGER  |\n"
-        "========================================\n"
-        "Select an option (1-4): "
-    );
-    scanf("%d",&who);
-
+        "========================================\n");
+    retry:
+        printf("Select an option (1-4): ");
+        scanf("%d",&who);
+    
+    //if the user input 5 or 0.
+    if(who!=1 && who!=2 && who!=3 && who!=4){
+        printf("Invalid Input!! Error!!\n");
+        goto retry;
+    }
+    
      printf(
         "\n------------ LOGIN PORTAL ------------\n");
 
@@ -40,19 +64,33 @@ int login(){
 
 
     if(strcmp(usr, "owner") == 0 && strcmp(pass, "owner_123") == 0){
-        printf("Succesfully LOGGED IN!!");
+        printf("Succesfully LOGGED IN!!\n");
     }
     else if(strcmp(usr, "chef") == 0 && strcmp(pass, "chef_123") == 0){
-        printf("Succesfully LOGGED IN!!");
+        printf("Succesfully LOGGED IN!!\n");
     }
     else if(strcmp(usr, "waiter") == 0 && strcmp(pass, "waiter_123") == 0){
-        printf("Succesfully LOGGED IN!!");
+        printf("Succesfully LOGGED IN!!\n");
     }
     else if(strcmp(usr, "table_man") == 0 && strcmp(pass, "table_man_123") == 0){
-        printf("Succesfully LOGGED IN!!");
+        printf("Succesfully LOGGED IN!!\n");
     }
     else{
-        printf("LOGIN CREDENTIALS are INCORRECT!!");
+        printf("LOGIN CREDENTIALS are INCORRECT!!\n");
     }
-    return who;
+
+    return who; //returns who has the person logged as.
+}
+
+int owner(){
+    
+}
+int waiter(){
+
+}
+int chef(){
+
+}
+int table_manager(){
+
 }
